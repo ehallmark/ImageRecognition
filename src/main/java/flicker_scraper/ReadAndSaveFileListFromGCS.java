de1f57ba722ef5c580a8a2dbf8fb8628dba8f43f
Evan Hallmark <ehallmar@usc.edu>
2017-03-18 09:50:36 -0700

    changes

--- src/main/java/flicker_scraper/ReadAndSaveFileListFromGCS.java
+++ src/main/java/flicker_scraper/ReadAndSaveFileListFromGCS.java
@@ -2,9 +2,7 @@ package main.java.flicker_scraper;
 
 import org.apache.commons.io.FileUtils;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -14,18 +12,26 @@ public class ReadAndSaveFileListFromGCS {
     public static File IMAGE_LOCATIONS_FILE = new File("/mnt/bucket/actual_image_locations.txt");
     public static void main(String[] args) throws Exception {
         File dir = new File(ScrapeImages.IMAGE_DIR);
+        File urls = MergeUrlFiles.mergedFile;
+        BufferedReader reader = new BufferedReader(new FileReader(urls));
         AtomicInteger cnt = new AtomicInteger(0);
         BufferedWriter writer = new BufferedWriter(new FileWriter(IMAGE_LOCATIONS_FILE));
-        FileUtils.iterateFiles(dir,new String[]{"jpg"},false).forEachRemaining(file->{
-            try {
-                writer.write(file.getName() + "\n");
-                writer.flush();
-                System.out.println(cnt.getAndIncrement());
-            } catch(Exception e) {
-                e.printStackTrace();
+        reader.lines().limit(4000000).forEach(line->{
+            int hash = line.hashCode();
+            String url = ScrapeImages.IMAGE_DIR+String.valueOf(hash)+".jpg";
+            if(new File(url).exists()) {
+                try {
+                    writer.write(url + "\n");
+                    writer.flush();
+                    System.out.println(cnt.getAndIncrement());
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
+
         System.out.println("Finished");
         writer.close();
+        reader.close();
     }
 }
