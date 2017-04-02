043b1e1dc31adbe32e4d7830c35c3931e7cb9b16
Evan Hallmark <ehallmar@usc.edu>
2017-04-02 10:22:30 -0700

    chagnes

--- src/main/java/flicker_scraper/IngestMNIST.java
+++ src/main/java/flicker_scraper/IngestMNIST.java
@@ -1,9 +1,11 @@
 package main.java.flicker_scraper;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.TypedColumn;
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.nd4j.linalg.dataset.DataSet;
 
@@ -47,5 +49,24 @@ public class IngestMNIST {
                 .format(FlickrScraper.AVRO_FORMAT)
                 .mode(SaveMode.Overwrite)
                 .save(FlickrScraper.LABELED_IMAGES_BUCKET+bucketName);
+    }
+
+    private static JavaRDD<DataSet> getData(String bucket, SparkSession spark) {
+        return spark.read()
+                .format(FlickrScraper.AVRO_FORMAT)
+                .load(FlickrScraper.LABELED_IMAGES_BUCKET+bucket)
+                .as(Encoders.bean(FeatureLabelPair.class))
+                .toJavaRDD()
+                .map(pair->{
+                    return new DataSet(pair.getFeatures(),pair.getLabels());
+                });
+    }
+    
+    public static JavaRDD<DataSet> getTrainData(SparkSession spark) {
+        return getData("mnist-train",spark);
+    }
+
+    public static JavaRDD<DataSet> getTestData(SparkSession spark) {
+        return getData("mnist-test",spark);
     }
 }
